6120b11fa84ad55f572eb1ee2fe31a6457f25c46
Anthony Williams <hi@antw.me>
2013-03-09 20:37:32 +0000

    Add Justice.balance.
    This is the main, public method for using Justice.

--- lib/justice.rb
+++ lib/justice.rb
@@ -8,6 +8,38 @@ require 'justice/variable'
 require 'justice/version'
 
 module Justice
+  # Public: Balances the given values.
+  #
+  # elements    - An array or hash containing hashes, each of which is item to
+  #               be used by the balancer to reach the equilibrium.
+  # equilibrium - The value to which you want the elements to sum.
+  #
+  # For example, with an array:
+  #
+  #   balancer = HashBalancer.new([
+  #     { min: 0, max: 100, value: 75, static: true },
+  #     { min: 0, max: 100, :value  0 }
+  #   ])
+  #
+  #   balancer.balanced
+  #   # => [ 75, 25 ]
+  #
+  # Or with a hash:
+  #
+  #   balancer = HashBalancer.new({
+  #     a: { min: 0, max: 100, value: 75, static: true },
+  #     b: { min: 0, max: 100, :value  0 }
+  #   })
+  #
+  #   balancer.balanced
+  #   # => { a: 75, b: 25 }
+  #
+  # Returns an array or hash, depending on what you provided.
+  def self.balance(values, equilibrium = 100)
+    klass = values.is_a?(Hash) ? HashBalancer : Balancer
+    klass.new(values, equilibrium).balanced
+  end
+
   # Internal: Given an object, tries to convert it to a BigDecimal.
   #
   # Returns a BigDecimal, or the +object+ if it could not be coerced.
@@ -21,4 +53,4 @@ module Justice
   def self.sum(values)
     values.inject(Justice.obj_to_d(0)) { |sum, value| sum + value }
   end
-end
+end # Justice
---
+++ spec/justice/justice_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Justice do
+  describe '.balance' do
+    context 'given an array of values' do
+      let(:values) do
+        Justice.balance([
+          { min: 0, max: 100, value: 50, static: true },
+          { min: 0, max: 100, value: 0 }
+        ])
+      end
+
+      it 'returns an array' do
+        expect(values).to be_an(Array)
+      end
+
+      it 'balances the values' do
+        expect(values).to eq([50, 50])
+      end
+    end # given an array of values
+
+    context 'given a hash of values' do
+      let(:values) do
+        Justice.balance({
+          a: { min: 0, max: 100, value: 50, static: true },
+          b: { min: 0, max: 100, value: 0 }
+        })
+      end
+
+      it 'returns a hash' do
+        expect(values).to be_a(Hash)
+      end
+
+      it 'balances the values' do
+        expect(values).to eq(a: 50, b: 50)
+      end
+    end # given an array of values
+  end # .balance
+end # Justice
